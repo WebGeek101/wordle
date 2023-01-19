45140258a1b6818755e3ba6a0a7d63c44081a0d3
Shockedreally <pavanmanjunath768@gmail.com>
2023-01-19 20:02:56 +0530

    Updated

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -13,7 +13,7 @@ export default function Keypad({ usedKeys }) {
 
   return (
     <div className="keypad">
-      {letters && letters.map(l => {/
+      {letters && letters.map(l => {
         const color = usedKeys[l.key]
         return (
           <div key={l.key} className={color}>{l.key}</div>
